e591df8edf426fa6b055daa022d3bbd710426e77
wayne <waynekim07@gmail.com>
2025-05-14 09:48:20 +0300

    Created a js file

---
+++ script.js
@@ -0,0 +1,13 @@
+// nav links
+const homeNav  = document.querySelector("#home");
+const tableNav  = document.querySelector("#table");
+const spreadSheetNav  = document.querySelector("#spreadSheet");
+const filterNav  = document.querySelector("#filter");
+
+// sections
+
+const homeSection  = document.querySelector("#section-home");
+const tableSection  = document.querySelector("#section-table");
+const spreadSheetSection  = document.querySelector("#section-spreadSheet");
+const filterSection  = document.querySelector("#section-filter");
+
