ad785818efc0974701c3d086828883de30d6d571
wayne <waynekim07@gmail.com>
2025-05-15 13:11:41 +0300

    Added the addTransaction function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 //variables
   //header heading
   const logo = document.querySelector("header .heading")
@@ -7,9 +8,30 @@
   // sections
   const sections = document.querySelectorAll(".section")
   const [homeSection,tableSection,spreadSheetSection,filterSection] = sections
+  //btns
+  const addTransactionBtn = document.querySelector("#add-transaction")
+  //table
+  const transactionTable = document.querySelector(".section-table table tBody")
+  //arrays
+  let transactions = [];
 
 //functions
 
+//renderPage = renders the page to the original set state
+function renderPage(){
+  // changeFocusNav(homeNav)
+  transactionTable.innerHTML = transactions.map(transaction => `
+    <tr>
+                            <td>${transaction.date.toDateString().slice(4)}</td>
+                            <td>${transaction.title}</td>
+                            <td>${transaction.type === "expense"? transaction.amount:"-"}</td>
+                            <td>${transaction.type === "income"? transaction.amount:"-"}</td>
+                        </tr>
+    `
+  ).join("")
+
+  
+}
 // changeFocusNav = adds the focus-nav class to the nav-link selected
   function changeFocusNav (e) {
     for(let nav of e.parentNode.querySelectorAll("li")){
@@ -33,15 +55,24 @@
     }
     
   }
-  //renderPage = renders the page to the original set state
-  function renderPage(){
-    changeFocusNav(homeNav);
-    
-  }
+  //addTransaction = adds a transaction to the trasactions array
+  function addTransaction(){
+    const[title,amount] = document.querySelectorAll(".inp input")
+    const type = document.querySelector(".inp select")
+    transactions.push({
+      date:new Date(),
+      title:title.value,
+      amount:Number(amount.value),
+      type:type.value,
+    });
+    console.log(transactions)
+    renderPage();
+  } 
 //add event listeners 
   //when a nav item is clicked it would evoke the changeFocusNav function on the item clicked
   navItems.forEach(item=>item.addEventListener("click",e=>changeFocusNav(e.target)))
   // when the logo is clicked it would evoke the changeFocusNav function on the homeNav
   logo.addEventListener("click",()=>changeFocusNav(homeNav))
-
-  renderPage()
+  //when the add button is clicked it would add a transaction to the treansactions array 
+  addTransactionBtn.addEventListener("click", addTransaction)
+renderPage()
